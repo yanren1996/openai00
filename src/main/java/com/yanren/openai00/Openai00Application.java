3cbbb7658946dcec31702a8d6ef26600e80d5b23
yanren <easy10441@gmail.com>
2024-08-13 09:57:44 +0800

    init

---
+++ src/main/java/com/yanren/openai00/Openai00Application.java
@@ -0,0 +1,13 @@
+package com.yanren.openai00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Openai00Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Openai00Application.class, args);
+    }
+
+}
