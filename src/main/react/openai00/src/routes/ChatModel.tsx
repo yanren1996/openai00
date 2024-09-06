5d734dc26bdb17ca8f2f3af96d4f89427b365e34
yanren <easy10441@gmail.com>
2024-09-06 14:15:01 +0800

    stream 還有小bug

--- src/main/react/openai00/src/routes/ChatModel.tsx
+++ src/main/react/openai00/src/routes/ChatModel.tsx
@@ -24,13 +24,51 @@ export default function () {
     // 取出user訊息先印出
     const newMsg = msg;
     setMsg('');
-    sethistory(pre => [...pre, { id: 'm_' + msgCount.current++, content: newMsg }]);
+    const current = msgCount.current++; //訊息計數器
+    sethistory(pre => [...pre, { id: 'm_' + current, content: newMsg }]);
     // 發送伺服器&印出ai回應
-    const ajax = await fetch(`http://localhost:8080/ai/chat-model${opt.isMetadata ? '/metadata' : ''}?message=${newMsg}`);
-    const text = await ajax.text();
-    sethistory(pre => [...pre, { id: 'm_' + msgCount.current++, content: text }])
+    if (!opt.isStream) {
+      await call(newMsg, opt.isMetadata);
+    } else {
+      stream(newMsg, opt.isMetadata);
+    }
   }
 
+  const call = async (newMsg: string, isMetadata: boolean) => {
+    const current = msgCount.current++;// 訊息計數器
+    if (!isMetadata) {
+      const ajax = await fetch(`http://localhost:8080/ai/chat-model?message=${newMsg}`);
+      const text = await ajax.text();
+      sethistory(pre => [...pre, { id: 'm_' + current, content: text }])
+    } else {
+      const ajax = await fetch(`http://localhost:8080/ai/chat-model/metadata?message=${newMsg}`);
+      const json = await ajax.json();
+      const text = JSON.stringify(json, null, 2);
+      sethistory(pre => [...pre, { id: 'm_' + current, content: text }])
+    }
+  }
+
+  const stream = (newMsg: string, isMetadata: boolean) => {
+    const current = msgCount.current++;// 訊息計數器
+    sethistory(pre => [...pre, { id: 'm_' + current, content: '' }]);
+    if (!isMetadata) {
+      const sse = new EventSource(`http://localhost:8080/ai/chat-model/stream?message=${newMsg}`);
+      sse.addEventListener('message', msg => {
+        if (msg.data === '%end%') { // ai 說完
+          sse.close();
+        } else { // ai 接續說
+          sethistory(pre => pre.map(item => {
+            if (item.id === 'm_'+ current) {
+              return {...item, content: item.content+=msg.data}
+            }
+            return item;
+          }));
+        }
+      })
+    } else {
+      // const sse = new EventSource(`localhost:8080/ai/chat-model/stream/metadata?message=${newMsg}`);
+    }
+  }
 
   return (<>
     <input value={msg} onChange={e => setMsg(e.target.value)} />
@@ -38,7 +76,7 @@ export default function () {
     <label><input type="checkbox" onChange={handleOptChange} checked={opt.isMetadata} name="isMetadata" />metadata</label>
     <button onClick={handleClick}>問ai</button>
     <div>
-      {history.map(item => <p key={item.id}>{item.content}</p>)}
+      {history.map(item => <pre key={item.id}>{item.content}</pre>)}
     </div>
   </>);
 }
